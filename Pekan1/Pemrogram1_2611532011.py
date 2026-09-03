4ef6d073c30f162dbc9efc89caa7d2a7a2263512
galid-ldd <mgalidavero003@gmail.com>
2026-09-03 12:24:11 +0700

    Praktikum pekan 1

---
+++ Pekan1/Pemrogram1_2611532011.py
@@ -0,0 +1,12 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+print("Hello World")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\\n" + "hildaameliawati_2611532011")
