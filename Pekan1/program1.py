c40ada58ece4b3bfbd148d0c31b95fa04a662ce3
Hilda Amelia Wati <hildaamelia0376@gmail.com>
2026-09-09 23:27:46 +0700

    Tambah program Praktikum Pekan 1

---
+++ Pekan1/program1.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+print("Hello wordl")
+print()
+print("Program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a/n")
+print("Hilda Amelia Wati dan 2611532011")
